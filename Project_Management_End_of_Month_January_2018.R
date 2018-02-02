e99af008197c859898937fb05640cf4c68ed0659
Jeff Cromwell, PhD <designofharmony@gmail.com>
2018-02-01 17:02:48 -0700

    Add files via upload

---
+++ Project_Management_End_of_Month_January_2018.R
@@ -0,0 +1,62 @@
+library(xtable)
+library(stringr)
+library(stringi)
+library(readr)
+library(readxl)
+
+#--------------------------------Data------------------------------------------------------------------------------------------------------
+
+
+Project.Management.End.of.Month.January.2018.Data.df <- as.data.frame(read_csv("January/Project_Management_End_of_Month_January_2018_Data.txt"))
+View(Project.Management.End.of.Month.January.2018.Data.df)
+
+
+Project.Names.List<-Project.Management.End.of.Month.January.2018.Data.df$ProjectName
+Project.Names.List.N<-length(Project.Names.List)
+
+#-------------------------------Analysis---------------------------------------------------------------------------------------------------
+
+Project.Management.End.of.Month.January.2018.Data.Group.1.df<-subset(Project.Management.End.of.Month.January.2018.Data.df,
+                                                                      Project.Management.End.of.Month.January.2018.Data.df$LectureID==1)
+Project.Management.End.of.Month.January.2018.Data.Group.2.df<-subset(Project.Management.End.of.Month.January.2018.Data.df,
+                                                                      Project.Management.End.of.Month.January.2018.Data.df$LectureID==2)
+Project.Management.End.of.Month.January.2018.Data.Group.3.df<-subset(Project.Management.End.of.Month.January.2018.Data.df,
+                                                                      Project.Management.End.of.Month.January.2018.Data.df$LectureID==3)
+Project.Management.End.of.Month.January.2018.Data.Group.4.df<-subset(Project.Management.End.of.Month.January.2018.Data.df,
+                                                                     Project.Management.End.of.Month.January.2018.Data.df$LectureID==4)
+
+Project.Totals.All.Groups<-sum(Project.Management.End.of.Month.January.2018.Data.df$Total)
+
+Project.Totals.All.Groups.1<-sum(Project.Management.End.of.Month.January.2018.Data.Group.1.df$Total)
+Project.Totals.All.Groups.2<-sum(Project.Management.End.of.Month.January.2018.Data.Group.2.df$Total)
+Project.Totals.All.Groups.3<-sum(Project.Management.End.of.Month.January.2018.Data.Group.3.df$Total)
+Project.Totals.All.Groups.4<-sum(Project.Management.End.of.Month.January.2018.Data.Group.4.df$Total)
+
+Table.Project.Totals.df<-data.frame()
+Table.Project.Totals.df<-cbind(Project.Totals.All.Groups.1,
+                               Project.Totals.All.Groups.2,
+                               Project.Totals.All.Groups.3,
+                               Project.Totals.All.Groups.4,
+                               Project.Totals.All.Groups)
+colnames(Table.Project.Totals.df)<-c("Group 1","Group 2","Group 3","Group 4","Total")
+
+#-------------------------------Tables---------------------------------------
+
+Table.1<-xtable(Table.Project.Totals.df)
+
+#-----------------------------Figures----------------------------------------
+
+Figure.1<-plot(Project.Management.End.of.Month.January.2018.Data.df$Total)
+
+par(mfrow = c(2,2), mar = c(2,2,2,2))
+Figure.2<-hist(Project.Management.End.of.Month.January.2018.Data.Group.1.df$Total)
+Figure.3<-hist(Project.Management.End.of.Month.January.2018.Data.Group.2.df$Total)
+Figure.4<-hist(Project.Management.End.of.Month.January.2018.Data.Group.3.df$Total)
+Figure.5<-hist(Project.Management.End.of.Month.January.2018.Data.Group.4.df$Total)
+
+#-----------------------------References---------------------------------------
+
+
+
+
+
